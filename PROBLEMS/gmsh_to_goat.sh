56d0a4b7d714d5accb2d9f2e4271e9a264707453
Milan Hanuš <Milan@dhcp-bizzell-41.as.tamu.edu>
2015-06-09 15:25:34 -0500

    Make gmsh_to_goat.sh oblivious to where it is being called from.

--- PROBLEMS/gmsh_to_goat.sh
+++ PROBLEMS/gmsh_to_goat.sh
@@ -4,7 +4,9 @@ if [ "$#" -gt 2 ] || [ "$#" -eq 0 ]; then
     echo "Illegal number of parameters"
 fi
 
+mydir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 problem=$1
+problem_dir=$mydir/$problem
 
 if [ "$#" -eq 2 ]; then
     mesh=$2
@@ -12,20 +14,20 @@ else
     mesh=$problem
 fi
 
-dos2unix $problem/$mesh.msh
+dos2unix $problem_dir/$mesh.msh
 
 if [ $? -ne 0 ]; then
     echo "Dos2unix not performed."
 fi
 
-dolfin-convert $problem/$mesh.msh $problem/$mesh.xml
+dolfin-convert $problem_dir/$mesh.msh $problem_dir/$mesh.xml
 
 if [ $? -ne 0 ]; then
     echo "Fatal error occured - GOAT meshes not produced."
     exit -1
 fi
 
-python get_material_names.py $problem -m $mesh
+python $mydir/get_material_names.py $problem -m $mesh
 
 if [ $? -ne 0 ]; then
     echo "Fatal error occured - GOAT meshes not produced."
